092787bf721309a95d292ff13fb31f32a22ddd7d
reshmak-pfw <kambr01@pfw.edu>
2023-04-02 10:17:43 -0400

    Added code for create in cshtml

---
+++ CalorieCounter/CalorieWebApp/Models/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Users
+{
+	public Users()
+	{
+
+		public int Id { get; set; }
+
+		public String Name { get; set; } = String.Empty;
+
+		public int Age { get; set; }
+
+		public int CurrentCalorieIntake { get; set; }
+
+		public String Gender { get; set; } = String.Empty;
+	}
+}
