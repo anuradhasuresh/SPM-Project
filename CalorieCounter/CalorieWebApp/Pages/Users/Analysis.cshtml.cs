81146eee7e15b7c1a17528ff70cc72ef30023da4
saketh6555 <122711359+saketh6555@users.noreply.github.com>
2023-04-03 03:35:52 -0400

    added analysis code

--- CalorieCounter/CalorieWebApp/Pages/Users/Analysis.cshtml.cs
+++ CalorieCounter/CalorieWebApp/Pages/Users/Analysis.cshtml.cs
@@ -3,9 +3,29 @@ namespace CalorieWebApp.Pages.Users
 {
 	public class Analysis
 	{
-		public Analysis()
-		{
-		}
+		// public Analysis()
+		// {
+		// }
+		public List<Users> Calorie = new();
+        public async void OnGet()
+        {
+            using (var client = new HttpClient())
+            {
+				client.BaseAddress = new Uri("https://localhost:5215");
+				var responseTask = client.GetAsync("Analysis/name");
+
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = await result.Content.ReadAsStringAsync();
+                    Calorie = JsonConvert.DeserializeObject<List<Users>>(readTask);
+
+                }
+            }
+        }
+
 	}
 }
 
